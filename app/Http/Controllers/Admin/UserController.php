b78e5209aac1910101ff462fc10e2f82d33c7cf2
TheRplima <therplima@gmail.com>
2022-04-22 17:47:58 -0300

    Creating our CRUD GUI for the admin panel - EP7

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -14,7 +15,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        dd('index method on user controller');
+        return view('admin.users.index', ['users' => User::all()]);
     }
 
     /**
