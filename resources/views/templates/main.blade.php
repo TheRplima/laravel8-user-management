b1384a820538667c293cca558d899277070602c4
TheRplima <therplima@gmail.com>
2022-04-23 17:30:27 -0300

    Fix the link to home (index) on logo and nav menu

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -18,7 +18,7 @@
 <body>
     <nav class="navbar navbar-expand-lg">
         <div class="container">
-            <a class="navbar-brand" href="#">{{ config('app.name', 'User Management System') }}</a>
+            <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'User Management System') }}</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -52,7 +52,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
+                        <a class="nav-link" href="{{ url('/') }}">Home</a>
                     </li>
                     @can('is-admin')
                     <li class="nav-item">
